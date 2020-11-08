f1b8eeef3570578af2a7738c6420587ab126dbc6
Gokul Madhusudhan <gokul99.gm@gmail.com>
2020-11-08 20:21:36 +0530

    reactNavigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import { View, Platform } from 'react-native'
 import Menu from './MenuComponent'
 import Dishdetail from './DishdetailComponent';
-import { createStackNavigator, } from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import Home from './HomeComponent'
 
 
 //pay attention to this createStackNavigator function usage
+//always put all screens in createStackNavigator cause it gives u menu bar
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
     Dishdetail: { screen: Dishdetail }
@@ -23,11 +26,43 @@ const MenuNavigator = createStackNavigator({
     }
 })
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home },
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+})
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+})
 
 class Main extends Component {
 
     render() {
-        const AppContainer = createAppContainer(MenuNavigator);
+        const AppContainer = createAppContainer(MainNavigator);
         return (
             <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
                 <AppContainer />
